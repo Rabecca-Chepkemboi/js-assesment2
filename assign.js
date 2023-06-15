0272c6f43aa4878fb842dc34ae4cbff42a0e4ebc
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-06-15 08:55:32 +0300

    done js assesment2

---
+++ assign.js
@@ -0,0 +1,99 @@
+
+class AncestralStories {                  //created a main cla ss oralStories
+    constructor(moralLesson, ageGroup, length) {
+        this.moralLesson = moralLesson;
+        this.ageGroup = ageGroup;
+        this.length = length;
+    }
+
+    findLength() {
+        return this.moralLesson.length;  //Iorder to know how long does one story take
+    }
+}
+
+class Story extends AncestralStories {
+    constructor(ageGroup, moralLesson) {
+        super(ageGroup, moralLesson);
+        this.moralLesson = moralLesson;
+        this.ageGroup = ageGroup
+    }
+}
+
+class StoryTeller {
+    constructor(tellersName) {
+        this.tellersName = tellersName
+    }
+
+    tellStory(OralStories) {
+        const OralStories = 4
+        if (OralStories <= 4){
+            console.log("The storyteller gave less stoties");
+        }
+        else{
+            console.log("The storyteller gave more stories");
+        }
+        
+    }
+}
+
+class Translator {
+    constructor(targetLanguage) {
+        this.targetLanguage = targetLanguage;
+    }
+}
+const targetLanguage = "lusungu";
+
+    if (targetLanguage = targetLanguage){
+        console.log("The language used was understood");
+    }
+    else{
+        console.log("The language used was not understood");
+    }
+
+
+
+//OUESTION2
+
+class Recipe{
+    constructor(uniqueIngredients, preparationTime, cookingMethod, nutritionalInformation){
+        this.uniqueIngredients = uniqueIngredients;
+        this.preparationTime = preparationTime;
+        this.cookingMethod = cookingMethod;
+        this.nutritionalInformation = nutritionalInformation;
+    }
+}
+      
+class MoroccanRecipe extends Recipe{
+
+    constructor (preparationTime, cookingMethod,spice_level){
+            ("Moroccos food recipes", ingredients, preparationTime, cookingMethod, nutritionalInfo)
+            this.spice_level = spice_level
+    }
+        displayRecipe(){
+            console.log("Moroccan food has a long cooking mehod and more ingredients");
+        }
+}    
+    
+class EthiopianRecipe extends Recipe{
+    constructor (preparationTime, cookingMethod,spice_level){
+            ("Ethiopia food recipes", ingredients, preparationTime, cookingMethod, nutritionalInfo)
+            this.spice_level = spice_level
+    }
+        displayRecipe(){
+            console.log("Ethiopian staple food is Injera has a long cooking methodis bitter");
+        }
+    }    
+        
+class NigerianRecipe extends Recipe{
+      constructor (preparationTime, cookingMethod,spice_level){
+            ("Nigeria food recipe", ingredients, preparationTime, cookingMethod, nutritionalInfo)
+            this.spice_level = spice_level
+    }
+        displayRecipe(){
+            console.log("Nigerian food has a short cooking method and less recipe");
+        }
+    }   
+
+
+//QUESTION3    
+
---
+++ assign.py
@@ -0,0 +1,83 @@
+
+class Ancestral_stories:                                  #created a class oralstories
+    def __init__(self, length, moral_lesson, age_group):
+        self.length = length
+        self.moral_lesson = moral_lesson
+        self.age_group = age_group
+
+    def find_length(self):
+        return len(self.moral_lessson)
+
+class Fable(Ancestral_stories):
+    def __init__(self, length, moral_lesson, age_group):
+        super().__init__(length, moral_lesson, age_group)
+        self.moral_lesson = moral_lesson
+
+class StoryTeller:
+    def __init__(self, tellers_name):
+        self.tellers_name = tellers_name
+
+    def tell_story(self, stories):
+        stories = 4
+
+        if stories <= 4:
+            return"The storyteller gave less stoties"
+        
+        else:
+            return "The storyteller gave more stories"
+            
+        
+    
+class Translator:
+    def __init__(self, target_language):
+        self.target_language = target_language
+        target_language = "lusungu"
+
+        if target_language != target_language:
+            return"The language used was understood"
+        
+        else:
+            return "The language used was not understood"
+
+
+
+#QUESTION2
+
+class Recipe:
+     def __init__(self, unique_ingredients, preparation_time, cooking_method, nutritional_information):
+            self.unique_ingredients = unique_ingredients
+            self.preparation_time = preparation_time
+            self.cooking_method = cooking_method
+            self.nutritional_information = nutritional_information
+    
+        
+class MoroccanRecipe(Recipe):
+        def __init__(self, ingredients, preparation_time, cooking_method, nutritional_info, spice_level):
+            super().__init__("Moroccos food recipes", ingredients, preparation_time, cooking_method, nutritional_info)
+            self.spice_level = spice_level
+    
+        def display_recipe(self):
+            super().display_recipe()
+            return "Moroccan food has a long cooking mehod and more ingredients"
+    
+    
+class EthiopianRecipe(Recipe):
+    def __init__(self,ingredients, preparation_time, cooking_method, nutritional_info, injera_required):
+            super().__init__("Ethiopia food recipes", ingredients, preparation_time, cooking_method, nutritional_info)
+            self.injera_required = injera_required
+    
+    def display_recipe(self):
+            super().display_recipe()
+            return "Ethiopian staple food is Injera has a long cooking methodis bitter"
+    
+    
+        
+class NigerianRecipe(Recipe):
+    def __init__(self,ingredients, preparation_time, cooking_method, nutritional_info, palm_oil_required):
+            super().__init__("Nigeria food recipe", ingredients, preparation_time, cooking_method, nutritional_info)
+            self.palm_oil_required = palm_oil_required
+    
+    def display_recipe(self):
+            super().display_recipe()
+            return " Nigerian food has a short cooking method and less recipe"
+
